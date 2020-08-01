11d44547b873ae0e80cf8c4e7cdbda7e1a0c60c3
an0malous <anomalous58@gmail.com>
2020-08-01 22:33:01 +0900

    hello world

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-//Import the react and ReactDOM Libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 
-//Create a react component
-const App = ()=>{
-    return <div>Hi There</div>
-}
 
-//Take the react component and show it on the screen
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
